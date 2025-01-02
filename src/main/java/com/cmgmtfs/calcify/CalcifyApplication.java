9f37ee74ac6307b92213a2720377012c790d82f9
xshlc <heechuw@gmail.com>
2025-01-02 18:50:10 -0500

    clean setup with spring initializr

---
+++ src/main/java/com/cmgmtfs/calcify/CalcifyApplication.java
@@ -0,0 +1,13 @@
+package com.cmgmtfs.calcify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcifyApplication.class, args);
+	}
+
+}
