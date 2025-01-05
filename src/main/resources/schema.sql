3d52b5725659ec1503134ce403a54ea3e3265ca9
xshlc <heechuw@gmail.com>
2025-01-04 21:43:24 -0500

    sql for tables Events and UserEvents

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -76,3 +76,31 @@ CREATE TABLE UserRoles
     CONSTRAINT UQ_UserRoles_User_Id UNIQUE (user_id),
     CONSTRAINT UQ_UserRoles_Role_Id UNIQUE (role_id)
 );
+
+###### ===== EVENTS ===== ######
+DROP TABLE IF EXISTS Events;
+
+CREATE TABLE Events
+(
+    id          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    type        VARCHAR(50)     NOT NULL CHECK (type IN
+                                                ('LOGIN_ATTEMPT', 'LOGIN_ATTEMPT_FAILURE', 'LOGIN_ATTEMPT_SUCCESS',
+                                                 'PROFILE_UPDATE', 'PROFILE_PICTURE_UPDATE', 'ROLE_UPDATE',
+                                                 'ACCOUNT_SETTINGS_UPDATE', 'PASSWORD_UPDATE', 'MFA_UPDATE')),
+    description VARCHAR(255)    NOT NULL,
+    CONSTRAINT UQ_Events_Type UNIQUE (type)
+);
+###### ===== USER EVENTS ===== ######
+DROP TABLE IF EXISTS UserEvents;
+
+CREATE TABLE UserEvents
+(
+    id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id    BIGINT UNSIGNED NOT NULL,
+    event_id   BIGINT UNSIGNED NOT NULL,
+    device     VARCHAR(100) DEFAULT NULL,
+    ip_address VARCHAR(100) DEFAULT NULL,
+    created_at DATETIME     DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES Events (id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
