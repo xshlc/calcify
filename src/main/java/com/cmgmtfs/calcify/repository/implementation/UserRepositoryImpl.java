0226d5a3944e8d683847612b87d8b1ffb37d7418
xshlc <heechuw@gmail.com>
2025-01-06 02:39:23 -0500

    setup for saving new user implementation

--- src/main/java/com/cmgmtfs/calcify/repository/implementation/UserRepositoryImpl.java
+++ src/main/java/com/cmgmtfs/calcify/repository/implementation/UserRepositoryImpl.java
@@ -5,18 +5,25 @@ import com.cmgmtfs.calcify.exception.ApiException;
 import com.cmgmtfs.calcify.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Objects.requireNonNull;
+
 @Repository
 @RequiredArgsConstructor
 @Slf4j
 public class UserRepositoryImpl<T extends User> implements UserRepository<T> {
-    private static final String COUNT_USER_EMAIL_QUERY = ;
+    private static final String COUNT_USER_EMAIL_QUERY = "";
+    private static final String INSERT_USER_QUERY = "";
     public final NamedParameterJdbcTemplate jdbcTemplate;
 
     /**
@@ -28,6 +35,15 @@ public class UserRepositoryImpl<T extends User> implements UserRepository<T> {
         // Check whether the email is unique
         if (getEmailCount(user.getEmail().trim().toLowerCase()) > 0) throw new ApiException("Email already in use. Please use a different email and try again."));
         // Save new user
+        try {
+            KeyHolder holder = new GeneratedKeyHolder();
+            SqlParameterSource parameters = getSqlParameterSource(user);
+            jdbcTemplate.update(INSERT_USER_QUERY, parameters, holder);
+            // requireNonNull is a static import
+            user.setId(requireNonNull(holder.getKey())
+                    .longValue());
+            roleRepository.addRoleToUser(user.getId(), ROLE_USER.name());
+        } catch (EmptyResultDataAccessException exception) {} catch (Exception exception) {}
         // Add role to the user
         // Send verification URL
         // Save URL and verification table
@@ -36,6 +52,7 @@ public class UserRepositoryImpl<T extends User> implements UserRepository<T> {
         // If any errors, throw exception with proper message
         return null;
     }
+
 
     /**
      * @param page 
@@ -77,5 +94,7 @@ public class UserRepositoryImpl<T extends User> implements UserRepository<T> {
     // Other methods
     private Integer getEmailCount(String email) {
         return jdbcTemplate.queryForObject(COUNT_USER_EMAIL_QUERY, Map.of("email",email), Integer.class);
+    }
+    private SqlParameterSource getSqlParameterSource(User user) {
     }
 }
