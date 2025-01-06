9be879f416672092cfa47fe11c133076193f312d
xshlc <heechuw@gmail.com>
2025-01-06 02:15:13 -0500

    user repository

---
+++ src/main/java/com/cmgmtfs/calcify/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package com.cmgmtfs.calcify.repository;
+
+import com.cmgmtfs.calcify.domain.User;
+
+import java.util.Collection;
+
+
+public interface UserRepository <T extends User>{
+    /* Basic CRUD operations */
+    // create
+    T create(T data);
+
+    // read
+    // read many
+    Collection<T> list(int page, int pageSize);
+    // read one
+    T get(Long id);
+
+    // update
+    T update(T data);
+
+    // delete
+    // returns a boolean so that we know if successful or not
+    boolean delete(Long id);
+}
