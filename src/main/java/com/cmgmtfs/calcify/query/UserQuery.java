7c39e1c89e63df9247b5b817b48f8a66f196847f
xshlc <heechuw@gmail.com>
2025-01-07 15:31:14 -0500

    user query sql statements

--- src/main/java/com/cmgmtfs/calcify/query/UserQuery.java
+++ src/main/java/com/cmgmtfs/calcify/query/UserQuery.java
@@ -1,7 +1,9 @@
 package com.cmgmtfs.calcify.query;
 
 public class UserQuery {
-    public static final String COUNT_USER_EMAIL_QUERY = "";
-    public static final String INSERT_USER_QUERY = "";
-    public static final String INSERT_ACCOUNT_VERIFICATION_URL_QUERY = "";
+    // jdbc uses 'VALUES (?, ?, ?)' for placeholders
+    // however, we need to use the parameters from UserRepositoryImpl here
+    public static final String INSERT_USER_QUERY = "INSERT INTO Users (first_name, last_name, email, password) VALUES (:firstName, :lastName, :email, :password)";
+    public static final String COUNT_USER_EMAIL_QUERY = "SELECT COUNT(*) FROM Users WHERE email = :email";
+    public static final String INSERT_ACCOUNT_VERIFICATION_URL_QUERY = "INSERT INTO AccountVerificationUrls (user_id, url) VALUES (:userId, :url)";
 }
