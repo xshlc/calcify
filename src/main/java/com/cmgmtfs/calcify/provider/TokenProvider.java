978928fd0ca67ca7438941cad220043fe0de2416
xshlc <heechuw@gmail.com>
2025-01-15 02:06:00 -0500

    create refresh token with auth0/java-jwt

--- src/main/java/com/cmgmtfs/calcify/provider/TokenProvider.java
+++ src/main/java/com/cmgmtfs/calcify/provider/TokenProvider.java
@@ -1,20 +1,32 @@
 package com.cmgmtfs.calcify.provider;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.cmgmtfs.calcify.domain.UserPrincipal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.List;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import static java.lang.System.currentTimeMillis;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 
 @Component
 public class TokenProvider {
     public static final String AUTHORITIES = "authorities";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
     private static final String C_MGMT_FS = "C_MGMT_FS";
     private static final String CUSTOMER_MANAGEMENT_SERVICE = "CUSTOMER_MANAGEMENT_SERVICE";
-    private static final long ACCESS_TOKEN_EXPIRATION_TIME = 432_000_000; // 5 days
+    private static final long ACCESS_TOKEN_EXPIRATION_TIME = 1_800_000; // 30 minutes
+    private static final long REFRESH_TOKEN_EXPIRATION_TIME = 432_000_000; // 5 days
+
     @Value("${jwt.secret}")
     private String secret;
 
@@ -29,7 +41,30 @@ public class TokenProvider {
                 .withArrayClaim(AUTHORITIES, claims)
                 .withExpiresAt(new Date(currentTimeMillis() * ACCESS_TOKEN_EXPIRATION_TIME))
                 .sign(
-                        Algorithm.HMAC512(secret.getBytes()));
+                        HMAC512(secret.getBytes()));
+    }
+
+    public String createRefreshToken(UserPrincipal userPrincipal) {
+        String[] claims = getClaimsFromUser(userPrincipal);
+        return JWT.create()
+                .withIssuer(C_MGMT_FS)
+                .withAudience(CUSTOMER_MANAGEMENT_SERVICE)
+                .withIssuedAt(new Date())
+                .withSubject(
+                        userPrincipal.getUsername())
+                .withExpiresAt(new Date(currentTimeMillis() * REFRESH_TOKEN_EXPIRATION_TIME))
+                .sign(
+                        HMAC512(secret.getBytes()));
+    }
+
+    public List<GrantedAuthority> getAuthorities(String token) {
+        String[] claims = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new).collect(toList());
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
     }
 
     private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
@@ -37,5 +72,16 @@ public class TokenProvider {
                 .stream()
                 .map(GrantedAuthority::getAuthority)
                 .toArray(String[]::new);
+    }
+
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
+        try {
+           Algorithm algorithm = HMAC512(secret);
+           verifier = JWT.require(algorithm).withIssuer(C_MGMT_FS).build();
+        } catch (JWTVerificationException e) {
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED, e);
+        }
+        return verifier;
     }
 }
