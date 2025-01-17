f456684e26cf838e89b3e8a676ed02be7ca33fad
xshlc <heechuw@gmail.com>
2025-01-16 22:13:44 -0500

    better annotations for the LoginForm

--- src/main/java/com/cmgmtfs/calcify/form/LoginForm.java
+++ src/main/java/com/cmgmtfs/calcify/form/LoginForm.java
@@ -1,12 +1,14 @@
 package com.cmgmtfs.calcify.form;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class LoginForm {
-    @NotEmpty
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Invalid email. Please enter a valid email address.")
     private String email;
-    @NotEmpty
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
