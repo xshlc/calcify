b33b79235b71f3fa6af3ec1f1cbd1f31b6e3487a
xshlc <heechuw@gmail.com>
2025-01-21 16:47:52 -0500

    exception handling for JWT decode, empty result data access, disabled, locked errors

--- src/main/java/com/cmgmtfs/calcify/exception/HandleException.java
+++ src/main/java/com/cmgmtfs/calcify/exception/HandleException.java
@@ -1,11 +1,17 @@
 package com.cmgmtfs.calcify.exception;
 
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.cmgmtfs.calcify.domain.HttpResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -136,7 +142,8 @@ public class HandleException extends ResponseEntityExceptionHandler implements E
                 HttpResponse.builder()
                         .timeStamp(now().toString())
                         .reason(exception.getMessage() != null ?
-                                (exception.getMessage().contains("expected 1, actual 0") ? "Record not found" : exception.getMessage())
+                                (exception.getMessage()
+                                        .contains("expected 1, actual 0") ? "Record not found" : exception.getMessage())
                                 : "Some error occurred")
                         .developerMessage(exception.getMessage())
                         .status(INTERNAL_SERVER_ERROR)
@@ -144,4 +151,96 @@ public class HandleException extends ResponseEntityExceptionHandler implements E
                         .build(), INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(JWTDecodeException.class)
+    public ResponseEntity<HttpResponse> exception(JWTDecodeException exception) {
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason("Could not decode the token")
+                        .developerMessage(exception.getMessage())
+                        .status(INTERNAL_SERVER_ERROR)
+                        .statusCode(INTERNAL_SERVER_ERROR.value())
+                        .build(), INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<HttpResponse> emptyResultDataAccessException(EmptyResultDataAccessException exception) {
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason(exception.getMessage()
+                                .contains("expected 1, actual 0") ? "Record not found" : exception.getMessage())
+                        .developerMessage(exception.getMessage())
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value())
+                        .build(), BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<HttpResponse> disabledException(DisabledException exception) {
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .developerMessage(exception.getMessage())
+                        //.reason(exception.getMessage() + ". Please check your email and verify your account.")
+                        .reason("User account is currently disabled")
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value())
+                        .build()
+                , BAD_REQUEST);
+    }
+
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<HttpResponse> lockedException(LockedException exception) {
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .developerMessage(exception.getMessage())
+                        //.reason(exception.getMessage() + ", too many failed attempts.")
+                        .reason("User account is currently locked")
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value())
+                        .build()
+                , BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<HttpResponse> dataAccessException(DataAccessException exception) {
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .reason(processErrorMessage(exception.getMessage()))
+                        .developerMessage(processErrorMessage(exception.getMessage()))
+                        .status(BAD_REQUEST)
+                        .statusCode(BAD_REQUEST.value())
+                        .build()
+                , BAD_REQUEST);
+    }
+
+    private ResponseEntity<HttpResponse> createErrorHttpResponse(HttpStatus httpStatus, String reason, Exception exception) {
+        return new ResponseEntity<>(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .developerMessage(exception.getMessage())
+                        .reason(reason)
+                        .status(httpStatus)
+                        .statusCode(httpStatus.value())
+                        .build()
+                , httpStatus);
+    }
+
+    private String processErrorMessage(String errorMessage) {
+        if (errorMessage != null) {
+            if (errorMessage.contains("Duplicate entry") && errorMessage.contains("AccountVerifications")) {
+                return "You already verified your account.";
+            }
+            if (errorMessage.contains("Duplicate entry") && errorMessage.contains("ResetPasswordVerifications")) {
+                return "We already sent you an email to reset your password.";
+            }
+            if (errorMessage.contains("Duplicate entry")) {
+                return "Duplicate entry. Please try again.";
+            }
+        }
+        return "Some error occurred";
+    }
 }
